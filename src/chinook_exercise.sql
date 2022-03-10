02cbf70a47947fed1b7298cb7004d99a60ae81e7
Kevin Williams <90599747+NooGames@users.noreply.github.com>
2022-03-10 14:01:05 +0100

    Adds Chinook Exercise

--- src/chinook_exercise.sql
+++ src/chinook_exercise.sql
@@ -6,27 +6,53 @@ Chinook
 					MySQL SQL Exercises
 
 1. How many songs are there in the playlist “Grunge”?
-    Select * FROM playlist WHERE name = 'Grunge'
+
+Select * FROM playlist WHERE name = 'Grunge'
 
 2. Show information about artists whose name includes the text “Jack” and about artists whose name includes the text
-    “John”, but not the text “Martin”.
-    SELECT * FROM artist
-    WHERE Name = 'Jack' & 'John' &! 'Martin';
+“John”, but not the text “Martin”.
+
+SELECT * FROM artist
+WHERE Name = 'Jack' & 'John' &! 'Martin';
 
 3. For each country where some invoice has been issued, show the total invoice monetary amount, but only for countries
 where at least $100 have been invoiced. Sort the information from higher to lower monetary amount.
 
+SELECT SUM(Total) AS monetaryAmount FROM invoice
+GROUP BY BillingCountry
+HAVING SUM(Total) >= 100
+ORDER BY monetaryAmount DESC;
 
 
 4. Get the phone number of the boss of those employees who have given support to clients who have bought some song
 composed by “Miles Davis” in “MPEG Audio File” format.
 
+SELECT DISTINCT e2.Phone FROM employee INNER JOIN employee e2 on employee.ReportsTo = e2.EmployeeId, customer, invoice, artist, mediatype
+WHERE MediaTypeId = 1
+AND artist.Name = 'Miles Davis'
+AND invoice.CustomerId = customer.CustomerId
+AND customer.SupportRepId = employee.EmployeeId;
+
 5. Show the information, without repeated records, of all albums that feature songs of the “Bossa Nova” genre whose
 title starts by the word “Samba”.
+
+SELECT DISTINCT Album.AlbumId, Title, ArtistId FROM Album, Track, Genre
+WHERE Track.GenreId = Genre.GenreId
+AND Genre.Name = 'Bossa Nova'
+AND Track.AlbumId = Album.AlbumId
+AND Track.Name LIKE 'Samba%';
 
 
 6. For each genre, show the average length of its songs in minutes (without indicating seconds). Use the headers “Genre”
 and “Minutes”, and include only genres that have any song longer than half an hour.
+
+SELECT Genre.Name AS 'Genre',
+TRUNCATE(AVG(Milliseconds/60000), 0) AS 'Minutes'
+FROM Track, Genre
+WHERE Milliseconds/60000 > 30
+AND Genre.GenreId = Track.GenreId
+GROUP BY Genre;
+
 
 7. How many client companies have no state?
 
--- src/library_exercise.sql
+++ src/library_exercise.sql
@@ -1,6 +1,7 @@
 1 Show the members under the name "Jens S." who were born before 1970 that became members of the library in 2013.
 
-2 Show those books that have not been published by the publishing companies with ID 15 and 32, except if they were published before 2000.
+2 Show those books that have not been published by the publishing companies with ID 15 and 32, except if they were
+published before 2000.
 
 3 Show the name and surname of the members who have a phone number, but no address.
 
@@ -12,45 +13,81 @@
 
 7 Modify the previous clause so that only those days where the member was loaned more than one book appear.
 
-8 Show all library members from the newest to the oldest. Those who became members on the same day will be sorted alphabetically (by surname and name) within that day.
+8 Show all library members from the newest to the oldest. Those who became members on the same day will be sorted
+alphabetically (by surname and name) within that day.
 
 9 Show the title of all books published by the publishing company with ID 32 along with their theme or themes.
 
-10 Show the name and surname of every author along with the number of books authored by them, but only for authors who have registered books on the database.
+10 Show the name and surname of every author along with the number of books authored by them, but only for authors who
+have registered books on the database.
 
-11 Show the name and surname of all the authors with published books along with the lowest publishing year for their books.
+11 Show the name and surname of all the authors with published books along with the lowest publishing year for their
+books.
 
 
-12 For each signature and loan date, show the title of the corresponding books and the name and surname of the member who had them loaned.
+12 For each signature and loan date, show the title of the corresponding books and the name and surname of the member
+who had them loaned.
 
 
 13 Repeat exercises 9 to 12 using the modern JOIN notation.
 
-14 Show all theme names along with the titles of their associated books. All themes must appear (even if there are no books for some particular themes). Sort by theme name.
+14 Show all theme names along with the titles of their associated books. All themes must appear (even if there are no
+books for some particular themes). Sort by theme name.
 
+    SELECT td.cName,
+           bd.cTitle
+    FROM ttheme AS td
+    JOIN tbook AS bd
+    ON bd.nBookID = nBookID
+    ORDER BY td.cName;
 
-15 Show the name and surname of all members who joined the library in 2013 along with the title of the books they took on loan during that same year. All members must be shown, even if they did not take any book on loan during 2013. Sort by member surname and name.
+15 Show the name and surname of all members who joined the library in 2013 along with the title of the books they took
+on loan during that same year. All members must be shown, even if they did not take any book on loan during 2013.
+Sort by member surname and name.
 
+SELECT cName, cSurname, cTitle
+FROM tmember, tbook, tbookcopy, tloan
+WHERE tbook.nBookID = tbookcopy.nBookID
+  AND tbookcopy.cSignature = tloan.cSignature
+  AND tloan.cCPR = tmember.cCPR
+  AND dNewMember BETWEEN '2013-01-01' AND '2013-12-31'
+  AND dLoan BETWEEN '2013-01-01' AND '2013-12-31';
 
-16 Show the name and surname of all authors along with their nationality or nationalities and the titles of their books. Every author must be shown, even though s/he has no registered books. Sort by author name and surname.
+SELECT cm.cName,
+       cm.cSurname,
+       cm.dNewMember,
+       cl.dloan,
+       cb.cTitle
+FROM tmember AS cm
+         JOIN tloan AS cl
+         JOIN tbook AS cb
+            ON cl.cCPR & cm.cCPR = cb.cTitle
+WHERE cm.dNewMember >= 2013-01-01 || cm.dNewMember
+ORDER BY cm.cname & cm.cSurname;
+
+16 Show the name and surname of all authors along with their nationality or nationalities and the titles of their books.
+Every author must be shown, even though s/he has no registered books. Sort by author name and surname.
 
 
 17 Show the title of those books which have had different editions published in both 1970 and 1989.
 
 
-18 Show the surname and name of all members who joined the library in December 2013 followed by the surname and name of those authors whose name is “William”.
+18 Show the surname and name of all members who joined the library in December 2013 followed by the surname and name of
+those authors whose name is “William”.
 
 
 19 Show the name and surname of the first chronological member of the library using subqueries.
 
 
-20 For each publishing year, show the number of book titles published by publishing companies from countries that constitute the nationality for at least three authors. Use subqueries.
+20 For each publishing year, show the number of book titles published by publishing companies from countries that
+constitute the nationality for at least three authors. Use subqueries.
 
 
 21 Show the name and country of all publishing companies with the headings "Name" and "Country".
 
 
-22 Show the titles of the books published between 1926 and 1978 that were not published by the publishing company with ID 32.
+22 Show the titles of the books published between 1926 and 1978 that were not published by the publishing company with
+ID 32.
 
 
 23 Show the name and surname of the members who joined the library after 2016 and have no address.
