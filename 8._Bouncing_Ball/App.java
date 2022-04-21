9b21478ad27b8b4c5d34080c1ed86bbee0c73da2
Kevin Williams <90599747+NooGames@users.noreply.github.com>
2022-04-21 09:12:47 +0200

    ORGANIZATION

---
+++ 8._Bouncing_Ball/App.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Basketball");
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
